0331cd633687856580556b3895a760bebb6b7991
Oscar <al44231@un.edu.mx>
2024-05-15 17:34:08 -0600

    Proyecto

---
+++ Razas/Egipcio.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace EjercicioGato
+{
+
+
+    public class Egipcio
+    {
+        
+        public Egipcio()
+        {
+            
+        }
+
+        /*PROPIEDADES*/
+        public decimal Peso { get; set; }
+
+        public string Tamaño { get; set; }
+
+        public string Color { get; set; }
+
+        public string Nombre { get; set; }
+
+
+        /*ACCIONES*/
+        public void Comer()
+        {
+
+        }
+
+        public void Correr()
+        {
+
+        }
+
+        public void Jugar()
+        {
+
+        }
+    }
+}
---
+++ Razas/Program.cs
@@ -0,0 +1,37 @@
+﻿using EjercicioGato;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Egipcio egipcio01= new Egipcio();
+        egipcio01.Nombre = "Pelusa";
+        egipcio01.Color = "Blanco";
+        egipcio01.Tamaño="S";
+        egipcio01.Peso= 80.5M;
+
+        egipcio01.Comer();
+        egipcio01.Jugar();
+        egipcio01.Correr();
+
+        Sianes sianes01 = new Sianes();
+        sianes01.Nombre ="Lucho";
+        sianes01.Color="Negro";
+        sianes01.Tamaño="G";
+        sianes01.Peso=90.2M;
+
+        sianes01.Comer();
+        sianes01.Jugar();
+        sianes01.Correr();
+        
+
+
+
+
+    }
+}
+
+
+/*Crear la clase chihuahua 
+con 4 propiedades que son tamaño , peso , color , nombre
+y que tenga el constructor default*/
